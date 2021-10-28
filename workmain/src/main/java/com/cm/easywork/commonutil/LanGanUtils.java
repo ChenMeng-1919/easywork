2e6e08028887bae10e06c3acbdf4683c51e1bb01
cm <chenmeng_1919@163.com>
2021-10-28 18:17:06 +0800

    确定分片数

--- workmain/src/main/java/com/cm/easywork/commonutil/LanGanUtils.java
+++ workmain/src/main/java/com/cm/easywork/commonutil/LanGanUtils.java
@@ -2,6 +2,7 @@ package com.cm.easywork.commonutil;
 
 import com.cm.easywork.entity.LanGanInputEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -15,21 +16,32 @@ public class LanGanUtils {
         int HGnumberOfShards = 0;
         //横杆长度
         double HGlength = 0;
+        ArrayList<Double> resultList = new ArrayList<>();
+        //确定分片数
         for (int i = 1; i < 50; i++) {
-            double v = (lanGanInputEntity.getLength() - 100 - Double.parseDouble(splitArgsList.get(3)[0]) * (i + 1)) / i;
-            if (v >= 1200) {
-                double v2 = (lanGanInputEntity.getLength() - 100 - Double.parseDouble(splitArgsList.get(3)[0]) * (i + 1-1)) / (i-1);
-                HGnumberOfShards = i-1;
+            double v = (lanGanInputEntity.getLength() - 100 - 100 - Double.parseDouble(splitArgsList.get(3)[0]) * (i + 1)) / i;
+            if (v <= 1200) {
+                HGnumberOfShards = i;
                 break;
-
             }
         }
+        //确定横杆长度的备选集合
         for (double j = 100; j > 90; j -= 0.5) {
-            double v = (lanGanInputEntity.getLength() - 100 - Double.parseDouble(splitArgsList.get(3)[0]) * (HGnumberOfShards + 1)) / HGnumberOfShards;
-            if (v % 10 == 0 || v % 10 == 5) {
-                HGlength = v;
+            double v = (lanGanInputEntity.getLength() - j - j - Double.parseDouble(splitArgsList.get(3)[0]) * (HGnumberOfShards + 1)) / HGnumberOfShards;
+            if ((int) v == v || v <= 1200) {
+                resultList.add(v);
+            }
+        }
+        //判断有无最优解
+        for (Double aDouble : resultList) {
+            if (aDouble % 10 == 0 || aDouble % 10 == 5) {
+                HGlength = aDouble;
                 break;
             }
+        }
+        //如果无最优解则取最大整数
+        if (HGlength == 0) {
+            HGlength = resultList.get(0);
         }
         double[] result = {HGnumberOfShards, HGlength};
         return result;
